65cc2128c57dc4f3665a6f5c027758fb16c8c790
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-16 01:14:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  
+
 end
 
 def summon_captain_planet(planeteer_calls)
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(word)
-  
+
 end
 
 def find_the_cheese(cheese)
