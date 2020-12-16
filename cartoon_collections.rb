a651b29b4e2c072b2ef125e4295e535616645e17
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-16 01:19:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|w| w.capitalize}
+  planeteer_calls.collect {|w| w.capitalize << "!"}
 end
 
 def long_planeteer_calls(word)
