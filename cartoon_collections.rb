48ecc60193f304875ed6caf95333e1709b8056dc
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-16 01:14:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,15 @@
 def roll_call_dwarves(dwarves)
-  dwarves.map.with_index do |name, index|
-    puts "#{index+1}. #{name}"
-  end
+  
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |call|
-    call.capitalize << "!"
-  end
+
 end
 
 def long_planeteer_calls(word)
-  word.any? { |call| call.length > 4}
+  
 end
 
 def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.detect {|type| cheese_types.include?(type)}
+  
 end
