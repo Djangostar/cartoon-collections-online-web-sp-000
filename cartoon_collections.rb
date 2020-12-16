1105b689760201739f91ab161f86139e45a526ce
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-16 01:28:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect {|w| w.capitalize << "!"}
 end
 
-def long_planeteer_calls(word)
-  word.all? {|num| num <= 4}
+def long_planeteer_calls(words)
+  words.all? {|word| word.length > 4}
 end
 
 def find_the_cheese(cheese)
